e11cd83f794283bf563d32768591ed9285dd87e7
Bryan Alcantara <alcantarabryan0418@gmail.com>
2024-04-02 15:22:23 -0400

    fnisihed project

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,11 @@
-export default function Progress() {
-  return null;
+export default function Progress({ReDonations}) {
+  
+  return (
+    <section className="progress">
+  <h2>
+    Raised <span className="secondary">{`$${ReDonations.reduce((acc,currVal)=> acc += currVal.amount,0)}`}</span> of
+    <span className="secondary">$1000</span>
+  </h2>
+</section>
+  )
 }
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,11 @@
-export default function RecentDonations() {
-  return null;
+function RecentDonations({ReDonations}) {
+  return (
+    <section>
+  <h2>Recent Donations</h2>
+  <ul>
+    {ReDonations.map(donor=><li><span>{`${donor.name} donated ${donor.amount}` }</span>{`${donor.caption}`}</li>)}
+  </ul>
+</section>
+  )
 }
+export default RecentDonations;
